96e651d432271d625014ae6a999cf52dbee1e6c8
hicham <hichamhajji545@gmail.com>
2026-09-11 20:10:09 -0700

    push

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.ani.ani"
+    namespace = "com.hichamhajji.anshow"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.ani.ani"
+        applicationId = "com.hichamhajji.anshow"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/hichamhajji/anshow/MainActivity.kt
+++ android/app/src/main/kotlin/com/hichamhajji/anshow/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ani.ani
+package com.hichamhajji.anshow
 
 import io.flutter.embedding.android.FlutterActivity
 
